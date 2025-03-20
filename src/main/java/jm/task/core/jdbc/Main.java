f3798d8d39ba7cca4d6b90581e1735d21fd3fda1
Sergey Romanovskikh <romanovskih.serega@yandex.ru>
2025-03-20 13:49:05 +0300

    task1 ready

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser("Dima", "Kekovich", (byte) 28);
+        userService.saveUser("Sergo", "Lolovich", (byte) 26);
+        userService.saveUser("Pacha", "Pucovich", (byte) 27);
+        userService.saveUser("Yan", "Pidorovich", (byte) 27);
+        System.out.println(userService.getAllUsers());
+        userService.removeUserById(1);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
+import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/kata_task1";
+    private final static String URLFIXED =
+            "jdbc:mysql://localhost:3306/kata_task1?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
+                    "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
+
+    private Connection connection;
+
+    public Util() {
+        try {
+            connection = DriverManager.getConnection(URLFIXED, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());;
+        }
+    }
+    public Connection getConnection() {
+        return connection;
+    }
 }
